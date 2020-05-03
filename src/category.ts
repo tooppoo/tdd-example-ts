69cb376a6a6e6095e6c2b63e9e9f6ceaf66c0338
philomagi <toooppoo56@gmail.com>
2020-05-03 16:08:23 +0900

    refactor test2

--- src/category.ts
+++ src/category.ts
@@ -1,7 +1,13 @@
 import { Product } from './product'
 
 export class Category {
-  contains(product: Product) {
-    return true
+  private products: Product[] = []
+
+  addProduct(product: Product): void {
+    this.products.push(product)
+  }
+
+  contains(product: Product): boolean {
+    return this.products.includes(product)
   }
 }
